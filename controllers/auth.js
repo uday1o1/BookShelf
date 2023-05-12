79e07842fe24211534f29a1575177eb0e98715e2
uday.arora <uday1o1arora@gmail.com>
2023-05-12 17:34:17 +0530

    auth works with bcrypt encryption

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,37 +1,71 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
+//setting salt round which tells bcrypt amount of time for pswd hashing
+const saltRounds = 10;
 
 exports.getLogin = (req, res, next) => {
-  console.log(req.session.loggedIn);
+  //flash msg is array of flashed strings, to send null if no flash so set value in variable if available
+  let errMessage = req.flash("error");
+  if (errMessage.length > 0) {
+    //latest msg in start of array
+    errMessage = errMessage[0];
+  } else {
+    //no error so no need to flash
+    errMessage = null;
+  }
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login Page",
     loggedIn: req.session.loggedIn,
+    errMessage: errMessage,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  // console.log(req.body.signInEmail, req.body.signInPassword)
-  // User.find({email: req.body.signInEmail, password: req.body.signInPassword})
-  User.findById("645a027e3b8a718194017040")
+  const signInEmail = req.body.signInEmail;
+  const signInPassword = req.body.signInPassword;
+
+  //if user found then check if entered pswd same a unhashed user pswd
+  User.findOne({ email: signInEmail })
     .then((user) => {
-      console.log(user)
-      //when logInPost req sent then session logIn key : true
-      req.session.loggedIn = true;
-      //add complete mongoose user instance to each user request(needed to access all user functions)
-      //add user to current session requests
-      req.session.user = user;
-      //redirect after session saved
-      //no need to use save always just when .then(func) needed
-      req.session.save((err) => {
-        if (err) {
-          console.log(err);
+      if (!user) {
+        req.flash("error", "Wrong email or password");
+        console.log("user not found");
+        return res.redirect("/login");
+      }
+      bcrypt.compare(signInPassword, user.password, function (err, result) {
+        //if pswd match
+        if (result) {
+          //when logInPost req sent then session logIn key : true
+          req.session.loggedIn = true;
+          //add complete mongoose user instance to each user request(needed to access all user functions)
+          //add user to current session requests
+          req.session.user = user;
+          //redirect after session saved
+          //no need to use save always just when .then(func) needed
+          req.session.save((err) => {
+            if (err) {
+              console.log(err);
+            }
+            console.log("logged in");
+            res.redirect("/");
+          });
         }
-        console.log("logged in");
-        res.redirect("/");
+        //if pswd not matched
+        else {
+          req.flash("error", "Pls enter the correct password");
+          console.log("wrong pswd");
+          return res.redirect("/login");
+        }
+        if (err) {
+          throw err;
+        }
       });
     })
     .catch((err) => {
-      console.log(err);
+      if (err) {
+        console.log(err);
+      }
     });
 };
 
@@ -48,18 +82,44 @@ exports.postLogout = (req, res, next) => {
 };
 
 exports.postSignIn = (req, res, next) => {
-  const user = new User({
-    name: req.body.signUpName,
-    email: req.body.signUpEmail,
-    password: req.body.signUpPassword,
-    cart: { products: [], totalPrice: 0 },
-  });
-  user
-    .save()
-    .then(() => {
-      console.log("new user created")
-      res.redirect("/login");
-      req.session.loggedIn = false;
+  const signUpName = req.body.signUpName;
+  const signUpEmail = req.body.signUpEmail;
+  const signUpPassword = req.body.signUpPassword;
+
+  //if user already exists
+  User.findOne({ email: signUpEmail })
+    .then((user) => {
+      if (user) {
+        req.flash(
+          "error",
+          "This user already exists either login or please sign up with another email id"
+        );
+        console.log("user already exists, pls login");
+        return res.redirect("/login");
+      }
+      //hash pswd using generated salt
+      bcrypt.hash(signUpPassword, saltRounds, function (err, hash) {
+        if (err) {
+          console.log(err);
+        }
+        //pswd saved is the new hashed pswd
+        const user = new User({
+          name: signUpName,
+          email: signUpEmail,
+          password: hash,
+          cart: { products: [], totalPrice: 0 },
+        });
+        user
+          .save()
+          .then(() => {
+            console.log("new user created");
+            req.session.loggedIn = false;
+            res.redirect("/login");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
     })
     .catch((err) => {
       console.log(err);
